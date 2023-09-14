98101e6cb8cc2653a1941394d1f909f0e44421d1
= <=>
2023-09-14 16:09:48 +0530

    change database to mysql

--- config/database.ts
+++ config/database.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT', 'mysql2');
 
   const connections = {
     mysql: {
