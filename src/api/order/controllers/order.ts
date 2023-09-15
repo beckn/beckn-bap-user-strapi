7c1ec87399abebadc1ada6036772b5cf2c48818a
= <=>
2023-09-15 16:31:51 +0530

    update order apis and schema

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -6,14 +6,24 @@ import { factories, Strapi } from '@strapi/strapi';
 
 export default factories.createCoreController('api::order.order', ({ strapi: Strapi }) => ({
     async find(ctx) {
-        ctx.query.filters = { ...ctx.query.filters, user_id: parseInt(ctx.state.user.id) }
+        ctx.query.filters = { ...ctx.query.filters, user: parseInt(ctx.state.user.id) }
         const { data, meta } = await super.find(ctx);
         return { data, meta };
     },
 
     async create(ctx) {
-        ctx.request.body.data = { ...ctx.request.body.data, user_id: ctx.state.user.id, publishedAt: new Date() }
-        console.log({ ...ctx.request.body, user_id: ctx.state.user.id, publishedAt: new Date() })
-        return await super.create(ctx);
+        ctx.request.body.data = strapi.service('api::order.order').buildData({ ...ctx.request.body, user: ctx.state.user.id });
+        return super.create(ctx);
+    },
+
+    async update(ctx) {
+        const order = await strapi.db.query('api::order.order').findOne({ where: { user: ctx.state.user, id: ctx.params.id, order_id: ctx.request.body.message.order.id }, });
+        if (!order) {
+            ctx.response.status = 404;
+            return;
+        }
+
+        ctx.request.body.data = strapi.service('api::order.order').buildData({ ...ctx.request.body, user: ctx.state.user.id });
+        return super.update(ctx);
     }
 }));
--- src/api/order/services/order.ts
+++ src/api/order/services/order.ts
@@ -2,6 +2,29 @@
  * order service
  */
 
-import { factories } from '@strapi/strapi';
+import { factories, Strapi } from '@strapi/strapi';
 
-export default factories.createCoreService('api::order.order');
+export default factories.createCoreService('api::order.order', ({ strapi: Strapi }) => ({
+    buildData(data) {
+        const { context, message: { order } } = data;
+        return {
+            ...data,
+            order_id: order.id,
+            bpp_id: context.bpp_id,
+            bpp_uri: context.bpp_uri,
+            currency: order.quote.price.currency,
+            delivery_status: order.state,
+            descriptor: order.provider.descriptor,
+            price: order.quote.price.value,
+            billing: order.billing,
+            fulfillments: order.fulfillments,
+            created_date: order.created_at,
+            last_updated_at: order.updated_at,
+            quote: order.quote,
+            transaction_id: context.transaction_id,
+            message_id: context.message_id,
+            payments: order.payments,
+            items: order.items,
+        };
+    }
+}));
